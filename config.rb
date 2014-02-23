8eb0d368865a0eaa318fa73661cd06182eb730b1
Seigo Uchida <spesnova@gmail.com>
2014-02-23 21:09:29 +0900

    Create source/posts directory to manage posts(articles) file.

--- config.rb
+++ config.rb
@@ -10,7 +10,7 @@ activate :blog do |blog|
 
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.sources = "/posts/{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
   #blog.layout = "layout"
   # blog.summary_separator = /(READMORE)/
